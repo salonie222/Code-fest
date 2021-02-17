2422f23526e529271a21aa09e5411114e67c270e
salonie <salonie693@gmail.com>
2021-02-17 13:41:37 +0530

    NeW update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   client_id: 'Cc604284bce32ecb12183caada32fed778e3819abb7411bacb8199d23b90567f1',
   client_secret: 'bb23d71833d5fa8746bdc17934a7125291432b2099572391b75f635687f1bfc4',
-  redirect_uri: 'https://salonie222.github.io/Code-fest',
+  redirect_uri: 'https://salonie222.github.io/Code-fest/',
   scope: 'spark:all spark:kms'
 };
